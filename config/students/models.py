9a20b7d47a512326b24a99dc485d0aef15ba7172
Angelina Molina <anm0124@auburn.edu>
2026-04-17 11:43:16 -0500

    added the nice layout and now just got to fix time and checkin updates

--- config/students/models.py
+++ config/students/models.py
@@ -12,5 +12,20 @@ class Student(models.Model):
     photo = models.ImageField(upload_to="student_photos/", blank=True, null=True)
     directives = models.TextField(blank=True)
 
+    # Parent / guardian 1
+    parent1_name = models.CharField(max_length=100 , blank=True, null=True)
+    parent1_email = models.EmailField(blank=True, null=False)
+    parent1_phone = models.CharField(max_length=20, blank=True, null=True)
+    parent1_sms_opt_in = models.BooleanField(default=False)
+
+
+      # Parent / guardian 2
+    parent2_name = models.CharField(max_length=100 , blank=True, null=True)
+    parent2_email = models.EmailField(blank=True, null=False)
+    parent2_phone = models.CharField(max_length=20, blank=True, null=True)
+    parent2_sms_opt_in = models.BooleanField(default=False)
+
+    #Medical / Health notes 
+    medical_notes = models.TextField(blank=True, null=True)
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.student_id})"
